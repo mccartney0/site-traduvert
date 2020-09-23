8fbcd7064f108ce01bcc8f3cb499694492d178f4
mccartney0 <traduvert@icloud.com>
2020-09-23 15:12:15 -0300

    Align form position center

--- index.php
+++ index.php
@@ -58,13 +58,14 @@
                                 <label for="nome" class="col-sm-4 col-form-label">
                                     <h2><svg width="15px" height="15px" viewBox="0 0 16 16" class="bi bi-star-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                        </svg> Entre na nossa lista!</h2>
+                                        </svg> Entre na nossa lista!
+                                    </h2>
                                 </label>
                                 <div class="col-sm-6">
-                                    <input type="text" name="nome" class="form-control" id="nome">
+                                    <input type="text" name="nome" class="form-control mb-2 " id="nome">
                                 </div>
                                 <div class="col-sm-2">
-                                    <button type="submit" class="btn btn-secondary mt-2 mb-2">Enviar</button>
+                                    <button type="submit" class="btn btn-secondary">Enviar</button>
                                 </div>
                             </div>
                         </form>
